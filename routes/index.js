dd86b248d93da65aa1c5f08320a5fba25cbe65c4
A.M.J <amy000245459@hotmail.com>
2023-11-22 03:41:57 +0800

    feat: error-handling

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user-controller')
-
+const { generalErrorHandler } = require('../../middleware/error-handler')
 
 router.post('/users', userController.addUser)
 router.get('/users', userController.getUsers)
 router.put('/users/:id', userController.updateUser)
 router.delete('/users/:id', userController.deleteUser)
-router.use('/', (req, res) => res.redirect('/users'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
