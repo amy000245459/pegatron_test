360c86bd75fcdf7a72f2b922db89286d2e569d63
A.M.J <amy000245459@hotmail.com>
2023-11-28 01:55:06 +0800

    feat: add picture model

--- models/picture.js
+++ models/picture.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-  name: {
-    type: String,
+  category: {
+    type: Schema.Types.ObjectId,
+    ref:'User',
+    index: true,
     required: true
-  },
-  age: {
-    type: Number,
+    },
+  picture: {
+    type: String,
     required: true
   }
 })
